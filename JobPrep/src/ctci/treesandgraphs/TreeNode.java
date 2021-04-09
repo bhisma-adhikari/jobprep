4f6ddd833ea41c5b57642b0ee20f242b3f48c91f
adhikab <adhikab@miamioh.edu>
2021-04-09 03:29:00 -0400

    updated TreeNode.java

--- JobPrep/src/ctci/treesandgraphs/TreeNode.java
+++ JobPrep/src/ctci/treesandgraphs/TreeNode.java
@@ -1,20 +1,21 @@
 // binary Tree node 
 
 //public methods: 
-//- getData() 
-//- setData() 
-//- getLeft() 
-//- setLeft()
-//- getRight()
-//- setRight() 
-//- printInOrder() 
-//- insert() 
-//- contains()
-//- remove() 
+// - getData() 
+// - setData() 
+// - getLeft() 
+// - setLeft()
+// - getRight()
+// - setRight() 
+// - toString() 
+// - printInOrder() 
+// - contains()
+// - getInOrderPredecessor() : returns the rightmost node of left subtree
+// - getInOrderSuccessor()   : returns the leftmost node of right subtree 
 
 package ctci.treesandgraphs;
 
-public class TreeNode<T extends Comparable<T>>{
+public class TreeNode<T extends Comparable<T>> {
 	private T data;
 	private TreeNode<T> left;
 	private TreeNode<T> right;
@@ -49,6 +50,11 @@ public class TreeNode<T extends Comparable<T>>{
 		this.right = right;
 	}
 
+	@Override
+	public String toString() {
+		return this == null ? null : this.data.toString();
+	}
+
 	public void printInorder() {
 		if (this.left != null) {
 			this.left.printInorder();
@@ -59,43 +65,35 @@ public class TreeNode<T extends Comparable<T>>{
 		}
 	}
 
-	public void insert(T data) {
-		if (data.compareTo(this.data) <= 0) {
-			if (this.left == null) {
-				this.left = new TreeNode<T>(data, null, null);
-			} else {
-				this.left.insert(data);
-			}
-		} else {
-			if (this.right == null) {
-				this.right = new TreeNode<T>(data, null, null);
-			} else {
-				this.right.insert(data);
-			}
-		}
+	// Time: O(n)
+	public boolean contains(T data) {
+		return this.data.equals(data) || (this.left != null && this.left.contains(data))
+				|| (this.right != null && this.right.contains(data));
+
 	}
 
-	public boolean contains(T data) {
-		if (data.equals(this.data)) {
-			return true;
-		} else if (data.compareTo(this.data) <= 0) {
-			if (this.left == null) {
-				return false;
-			}
-			return this.left.contains(data);
-		} else {
-			if (this.right == null) {
-				return false;
-			}
-			return this.right.contains(data);
+	// returns the rightmost node of left subtree
+	public TreeNode<T> getInOrderPredessor() {
+		if (this.left == null) {
+			return null;
 		}
+		TreeNode<T> curr = this.left;
+		while (curr.right != null) {
+			curr = curr.right;
+		}
+		return curr;
 	}
-	
-	// removes the first element (starting from root) that matches data
-	// returns true if an element is removed, else returns false 
-	// if ensureMaxHeap is true, the 
-	public boolean remove (T data, boolean ensureMinHeap) {
-		
+
+	// returns the leftmost node of right subtree
+	public TreeNode<T> getInOrderSuccessor() {
+		if (this.right == null) {
+			return null;
+		}
+		TreeNode<T> curr = this.right;
+		while (curr.left != null) {
+			curr = curr.left;
+		}
+		return curr;
 	}
 
 }
